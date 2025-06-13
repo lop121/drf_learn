df273cd09dcd32b6ff0c115d66c9d6fc8d394573
ilya <tor-yo@ya.ru>
2025-06-14 03:03:39 +0800

    pagination

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from women.views import *
 
 urlpatterns = [
@@ -27,4 +28,7 @@ urlpatterns = [
     path("api/v1/womendelete/<int:pk>/", WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .models import Category, Women
@@ -6,10 +7,17 @@ from .serializers import WomenSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
