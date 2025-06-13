52145a52ae74212a8e83f85d27556943e1189946
ilya <tor-yo@ya.ru>
2025-06-13 23:27:43 +0800

    permissions2

--- drfsite/women/permissions.py
+++ drfsite/women/permissions.py
@@ -10,7 +10,6 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
