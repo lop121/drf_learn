f5a42cb873aadee93fec8da75f067027563fa43b
ilya <tor-yo@ya.ru>
2025-06-14 00:26:24 +0800

    djoser

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,14 +1,9 @@
-from django.forms import model_to_dict
-from django.shortcuts import render
 from rest_framework import generics, viewsets
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .models import Category, Women
 from .serializers import WomenSerializer
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
 
 class WomenAPIList(generics.ListCreateAPIView):
@@ -17,10 +12,10 @@ class WomenAPIList(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
-class WomenAPIUpdate(generics.UpdateAPIView):
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
 
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
